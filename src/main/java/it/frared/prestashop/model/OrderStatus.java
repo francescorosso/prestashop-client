8310eceb5f65ba38860b43bdbf322a9630f5b55e
Francesco Rosso <francesco.rosso@agrimontana.it>
2023-11-03 14:22:16 +0100

    project config

---
+++ src/main/java/it/frared/prestashop/model/OrderStatus.java
@@ -0,0 +1,9 @@
+package it.frared.prestashop.model;
+
+public class OrderStatus {
+
+	public static final int PREPARAZIONE_IN_CORSO = 3;
+	public static final int SPEDITO = 4;
+	public static final int CONSEGNATO = 5;
+	public static final int INVIATO_A_GESTIONALE = 31;
+}
