e79e16b616f002df85b8cfee47639bba81739dbe
Francesco Rosso <francesco.rosso@agrimontana.it>
2024-01-15 18:01:40 +0100

    aggiunti order status

--- src/main/java/it/frared/prestashop/model/OrderStatus.java
+++ src/main/java/it/frared/prestashop/model/OrderStatus.java
@@ -6,4 +6,6 @@ public class OrderStatus {
 	public static final String PREPARAZIONE_IN_CORSO = "3";
 	public static final String SPEDITO = "4";
 	public static final String CONSEGNATO = "5";
+	public static final String ANNULLATO = "6";
+	public static final String IN_ATTESA_ACQUISIZIONE = "19";
 }
